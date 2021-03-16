76279c404a3a9e84609a95a4ecd0331b701f8144
ngocthinh1126 <78062673+ngocthinh1126@users.noreply.github.com>
2021-03-16 11:52:43 +0700

    Add files via upload

---
+++ SQLQuery_Cac phep toan.sql
@@ -0,0 +1,16 @@
+﻿--1. Tạo 1 bảng mới
+
+--2. Cho biết danh sách các đội của tổ chức 'tlus'
+select MADOI from DOI where MATOCHUC='tlus'
+union
+select madoi from DOIDUBI where MATOCHUC='tlus'
+
+--3. Cho biết ds các đội mà có tham gia giải đấu
+select MADOI from DOI
+intersect
+select madoi from THAMGIAGIAIDAU 
+--4. Cho biết ds các đội chưa tham gia giải đấu bao giờ
+select MADOI from DOI
+except
+select madoi from THAMGIAGIAIDAU
+
